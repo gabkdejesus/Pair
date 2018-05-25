f058288745ca7de7d00cf5db8c0f75e13c20a56c
Gab De Jesus <gabkdejesus@gmail.com>
2018-05-25 23:57:45 +0900

    Fix static_dir

--- pair/settings.py
+++ pair/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
